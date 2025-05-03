a9cc67b4a5ae599f49cb38f633bd86a45dd8db93
taivo <tai@localhost>
2025-05-03 01:30:52 -0700

    clean up drizzle.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,24 +1,28 @@
 import { defineConfig } from "drizzle-kit"
 import { getLocalD1File, getWranglerD1Config } from "~/lib/cf-utils"
 
-// biome-ignore lint/nursery/noProcessEnv: <explanation>
-const { CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_D1_TOKEN } = process.env
-const CLOUDFLARE_D1_ID = CLOUDFLARE_ACCOUNT_ID ? getWranglerD1Config("DB").database_id : null
-
-const isRemote = CLOUDFLARE_ACCOUNT_ID && CLOUDFLARE_D1_TOKEN && CLOUDFLARE_D1_ID
-
-if (isRemote) {
-	console.info("Env for remote D1 detected. Using Cloudflare D1 database specified in wrangler.jsonc")
-} else {
-	console.info("Local mode: using miniflare D1 database")
-}
-
 export default defineConfig({
 	out: "./migrations",
 	schema: "./src/schema",
 	dialect: "sqlite",
 
-	...(isRemote
+	...getCredentials(),
+})
+
+function getCredentials() {
+	// biome-ignore lint/nursery/noProcessEnv: <explanation>
+	const { CLOUDFLARE_ACCOUNT_ID, CLOUDFLARE_D1_TOKEN } = process.env
+	const CLOUDFLARE_D1_ID = CLOUDFLARE_ACCOUNT_ID ? getWranglerD1Config("DB").database_id : null
+
+	const isRemote = CLOUDFLARE_ACCOUNT_ID && CLOUDFLARE_D1_TOKEN && CLOUDFLARE_D1_ID
+
+	if (isRemote) {
+		console.info("Env for remote D1 detected. Using Cloudflare D1 database specified in wrangler.jsonc")
+	} else {
+		console.info("Local mode: using miniflare D1 database")
+	}
+
+	return isRemote
 		? {
 			driver: "d1-http",
 			dbCredentials: {
@@ -31,5 +35,5 @@ export default defineConfig({
 			dbCredentials: {
 				url: `file:${getLocalD1File()}`,
 			},
-		}),
-})
+		}
+}
