24fb1803d532ea9de7d622caff3e99d2c6ada606
taivo <tai@localhost>
2025-05-02 17:58:56 -0700

    setup vite-doctest-plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
-import solid from "vite-plugin-solid";
-import { defineConfig } from "vitest/config";
+import { doctest } from 'vite-plugin-doctest'
+import solid from "vite-plugin-solid"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
-  plugins: [solid()],
-  resolve: {
-    conditions: ["development", "browser"],
-  },
-});
+	plugins: [solid(), tsconfigPaths(), doctest()],
+	resolve: {
+		conditions: ["development", "browser"],
+	},
+	test: {
+		includeSource: [
+			'./src/**/*.{js,jsx,ts,tsx,mjs,mts}',
+		],
+	},
+})
